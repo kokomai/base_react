e1369491962e70527116a41b20050d5256fb13ba
coding-orca <kokomai6636@gmail.com>
2023-02-16 11:25:35 +0900

    add loading spinner

--- src/fragments/loading/Loading.js
+++ src/fragments/loading/Loading.js
@@ -1,3 +1,4 @@
+import { Backdrop, CircularProgress } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectLoading } from './loadingSlice';
@@ -6,13 +7,12 @@ import { selectLoading } from './loadingSlice';
 function Loading() {
     const loading = useSelector(selectLoading);
     return (
-        loading 
-        ?
-            <div className='loading'>
-                now Loading...
-            </div>
-        : 
-            <></>
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={loading}
+        >
+            <CircularProgress color="inherit" />
+        </Backdrop>
     );
 }
 
