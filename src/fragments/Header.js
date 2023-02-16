caf7e65bd8bcd456d48d1d0ccad4c8472648be7f
coding-orca <kokomai6636@gmail.com>
2023-02-16 17:48:00 +0900

    remove annotation

--- src/fragments/Header.js
+++ src/fragments/Header.js
@@ -13,24 +13,6 @@ import { Menu as MenuIcon} from '@mui/icons-material';
 
 function Header({open, toggleDrawer}) {
 
-    // const AppBar = styled(MuiAppBar, {
-    //     shouldForwardProp: (prop) => prop !== 'open',
-    //   })(({ theme, open }) => ({
-    //     zIndex: theme.zIndex.drawer + 1,
-    //     transition: theme.transitions.create(['width', 'margin'], {
-    //       easing: theme.transitions.easing.sharp,
-    //       duration: theme.transitions.duration.leavingScreen,
-    //     }),
-    //     ...(open && {
-    //       marginLeft: drawerWidth,
-    //       width: `calc(100% - ${drawerWidth}px)`,
-    //       transition: theme.transitions.create(['width', 'margin'], {
-    //         easing: theme.transitions.easing.sharp,
-    //         duration: theme.transitions.duration.enteringScreen,
-    //       }),
-    //     }),
-    //   }));
-
     return (
         <AppBar position="absolute" open={open}>
             <Toolbar
